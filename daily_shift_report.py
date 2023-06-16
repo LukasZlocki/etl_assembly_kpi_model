e51e89227b28a884e7d57a2c0862d82e89518a36
lzlocki <lzlocki@poczta.fm>
2023-06-16 15:58:24 +0200

    Add logic responsible for transforming data to daily shit raport

--- daily_shift_report.py
+++ daily_shift_report.py
@@ -12,32 +12,25 @@ class ShiftReport:
         self.__shift_II = ["1400-1500", "1500-1600", "1600-1700", "1700-1800", "1800-1900", "1900-2000", "2000-2100", "2100-2200"]
         self.__shift_III = ["2200-2300", "2300-0000", "0000-0100", "0100-0200", "0200-0300", "0300-0400", "0400-0500", "0500-0600"]
 
-
-
-
     # Extract shifts in date given
-    # Format [day, shift, output planned, output real, oee]
-    def calculate_shifts_raport_for_given_day(self, day):
-        shift_I_hourly_target = []
-        shift_II_hourly_target = []
-        shift_III_hourly_target = []
+    # Format [day, shift_number, output_planned, output real, oee]
+    def calculate_shifts_raport_for_given_day_and_shift(self, day, shift_number):
+        shift_hourly_target = []
         # invoke def to calculate hourly targets for needed shift
-        shift_I_hourly_target = self.extract_shift_hourly_targets_based_on_shift_hours_set(day, self.__shift_I,1)
-        #shift_II_hourly_targe = self.extract_shift_hourly_targets_based_on_shift_hours_set(self, day, self.__shift_II,2)
-        #shift_III_hourly_targe = self.extract_shift_hourly_targets_based_on_shift_hours_set(self, day, self.__shift_III,3)       
-        
-        hour_target_planed = 0
-        hourly_target_real = 0
-        for element in shift_I_hourly_target:
-            hour_target_planed = hour_target_planed + element[2]
-            hourly_target_real = hourly_target_real + element[3] 
-        oee = self.calculate_oee(hour_target_planed, hourly_target_real)
-        final = [day, 1, hour_target_planed, hourly_target_real, oee] 
-        return final
+        if shift_number == 1:
+            shift_hourly_target = self.extract_shift_hourly_targets_based_on_shift_hours_set(day, self.__shift_I,1)
+        if shift_number == 2:
+            shift_hourly_target = self.extract_shift_hourly_targets_based_on_shift_hours_set(day, self.__shift_II,2)
+        if shift_number == 3:
+            shift_hourly_target = self.extract_shift_hourly_targets_based_on_shift_hours_set(day, self.__shift_III,3)
+        return shift_hourly_target
 
     # Calculate oee for given data. Return value raunded to two digits
-    def calculate_oee(self, planned_output, real_output):
-        percentage_result = round(real_output*100/planned_output,2)
+    def __calculate_oee(self, planned_output, real_output):
+        if (planned_output == 0 and real_output == 0):
+            percentage_result = 0
+        else:
+            percentage_result = round(real_output*100/planned_output,2)
         return percentage_result
 
     def extract_shift_hourly_targets_based_on_shift_hours_set(self, day, shift_hours_seter, shift_number):
@@ -47,13 +40,13 @@ class ShiftReport:
         oee = 0
         for element in self.__dataset:
             if element[1]== day:
-                for timeframe in self.__shift_I:
+                for timeframe in shift_hours_seter:
                     if element[2] == timeframe:
                         shift_planned_output = shift_planned_output + int(element[3])
                         shift_output_real = shift_output_real + int(element[4])
-            if (shift_planned_output > 0):
-                oee = self.calculate_oee(shift_planned_output, shift_output_real)
-                shift_raport.append([day, shift_number, shift_planned_output , shift_output_real, oee])
-                shift_output_real = 0
-                shift_planned_output = 0           
+        if (shift_planned_output > 0):
+            oee = self.__calculate_oee(shift_planned_output, shift_output_real)
+            shift_raport.append([day, shift_number, shift_planned_output , shift_output_real, oee])
+            shift_output_real = 0
+            shift_planned_output = 0           
         return shift_raport
--- report.py
+++ report.py
@@ -5,34 +5,46 @@ import csv
 
 class Report:
     def __init__(self):
-        self.__hourly_target_raports
+        self.__hourly_target_raports = []
 
+        self.__source_path = source_path = "./src/"
+        self.__output_path = output_path = "./temp/"
 
-    
+        self.__jedox_file_name = jedox_file_name = "jedox_new.xlsm"
+        self.__hourly_raport_dataset_file = hourly_raport_dataset_file = "hourly_raport_dataset.csv"
+
     # EXTRACT horly target raports list from jedox file
-    def extract_hourly_target_raport_from_jedox_file(self, jedox_path, file_name):
+    def extract_hourly_target_raport_from_jedox_file(self):
         # Reading hourly target data sheet
-        hourly_targets_data_sheet = self.__read_jedox_hourly_targets_sheet(jedox_path + file_name)
+        hourly_targets_data_sheet = self.__read_jedox_hourly_targets_sheet(self.__source_path + self.__jedox_file_name)
         # Removing hours, minutes from dates
         hourly_targets_data_sheet = self.__remove_hours_minutes_from_date(hourly_targets_data_sheet, "Date")
         # Storing clear raport list in this class field
         self.__hourly_target_raports = hourly_targets_data_sheet
     
+    # GET hourly report from csv file
+    def get_hourly_dataset_from_csv_file(self):
+        path_with_filename = self.__output_path + self.__hourly_raport_dataset_file
+        with open(path_with_filename, "r") as file:
+            reader_csv = csv.reader(file)
+            list_of_csv = list(reader_csv)
+        return list_of_csv
+
     # GET hourly target raports list
     def get_hourly_target_raports(self):
         return self.__hourly_target_raports
 
     # SAVE hourly targer raports list to csv file
-    def save_raports_to_csv_file(self, path, filename):
-        self.__hourly_target_raports_list.to_csv(path+filename)
+    def save_raports_to_csv_file(self):
+        self.__hourly_target_raports.to_csv(self.__output_path + self.__hourly_raport_dataset_file)
 
     # LOAD hourly target raports from csv file and store it in variable
     def load_hourly_targets_raports_from_csv_file(self, path, filename):
         self.__hourly_target_raports = pd.read_csv(path+filename)
         return self.__hourly_target_raports
 
-    # read pure data from jedox file
-    def __read_jedox_hourly_targets_sheet(path):
+    # READ pure data from jedox file
+    def __read_jedox_hourly_targets_sheet(self, path):
         column_numbers = [3, 1, 4, 5]
         xls = pd.ExcelFile(path)
         data = pd.read_excel(
@@ -43,15 +55,11 @@ class Report:
         return data
 
     # removing hours, minutes from given set of dates
-    def __remove_hours_minutes_from_date(data_set, column_name):
+    def __remove_hours_minutes_from_date(self, data_set, column_name):
         data_set[column_name] = data_set[column_name].dt.date
         return data_set
 
-    def read_csv_dataset_no_pandas(path_with_filename):
-        with open(path_with_filename, "r") as file:
-            reader_csv = csv.reader(file)
-            list_of_csv = list(reader_csv)
-        return list_of_csv
+
     
     # Calculate oee for given data. Return value raunded to two digits
     def calculate_oee(self, planned_output, real_output):
